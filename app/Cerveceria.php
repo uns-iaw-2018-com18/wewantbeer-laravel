7e049ac5a4f3ea518f0faa6721d0855cca153982
Rodo93 <rodrigo102195@gmail.com>
2018-05-28 20:58:17 -0300

    Agrega la latitud y longitud a partir de la dirección pasada al servidor. Falta solucionar el problema de los nulls.

--- app/Cerveceria.php
+++ app/Cerveceria.php
@@ -4,5 +4,5 @@ use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 class Cerveceria extends Eloquent
 {
     protected $collection = 'cervecerias';
-    protected $fillable = ['id','nombre','direccion','telefono','web','email','sumaPuntajes','cantidadPuntajes','horario','happyHour','logo','foto','facebook','instagram'];
+    protected $fillable = ['id','nombre','direccion','telefono','web','email','sumaPuntajes','cantidadPuntajes','horario','happyHour','latLong','logo','foto','facebook','instagram'];
 }
--- app/Http/Controllers/AddController.php
+++ app/Http/Controllers/AddController.php
@@ -71,6 +71,17 @@ class AddController extends Controller
         $horarios[6] = "Cerrado";
       }
       $cerveceria->horario=$horarios;
+
+      // Get lat and long by address
+      $address = $request->direccion.", Bahía Blanca"; // Google HQ
+      $prepAddr = str_replace(' ','+',$address);
+      $geocode=file_get_contents('https://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&sensor=false');
+      $output= json_decode($geocode);
+      if(isset($output->results)){
+        $latitude = $output->results[0]->geometry->location->lat;
+        $longitude = $output->results[0]->geometry->location->lng;
+        $cerveceria->latLong = array($latitude,$longitude);
+     }
       $cerveceria->save();
       return 'Saved';
     }
