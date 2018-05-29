502fb4d6d2a1ce79a891e3641e55979ee1848d8e
DamianNud <DamianNud@users.noreply.github.com>
2018-05-29 01:59:38 -0300

    Algunas correcciones

--- app/Http/Controllers/AddController.php
+++ app/Http/Controllers/AddController.php
@@ -6,86 +6,88 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Cerveceria;
 
-class AddController extends Controller
-{
+class AddController extends Controller {
     /**
      * Show the profile for the given user.
      *
      * @param  int  $id
      * @return Response
      */
-    public function add(Request $request)
-    {
+    public function add(Request $request) {
+      // Falta chequear si los campos estan vacios
+      // En ese caso no hay que agregarlos
+
       $cerveceria = new Cerveceria();
+      $cerveceria->id = strtolower(preg_replace("/\s+/", "_", $request->nombre));
       $cerveceria->nombre = $request->nombre;
-      $cerveceria->id = $request->nombre;
       $cerveceria->direccion = $request->direccion;
-      $tipoTelefono=$request->tipoTel;
-      if($tipoTelefono == "0"){
-        $cerveceria->telefono ="+54291".$request->telefono;
-      }else{
-        $cerveceria->telefono ="+549291".$request->telefono;
+      $tipoTelefono = $request->tipoTel;
+      if ($tipoTelefono == "0") {
+        $cerveceria->telefono = "+54291".$request->telefono;
+      } else {
+        $cerveceria->telefono = "+549291".$request->telefono;
       }
-      $cerveceria->web=$request->web;
-      $cerveceria->email=$request->email;
+      $cerveceria->web = $request->web;
+      $cerveceria->email = $request->email;
       $cerveceria->facebook = $request->facebook;
       $cerveceria->instagram = $request->instagram;
-      if(isset($request->happyCheck)){
+      if (isset($request->happyCheck)) {
         $cerveceria->happyHour = $request->happyOpen."-".$request->happyClose;
       }
       $horarios = array();
       //ahora para cada uno de los dias tengo que agregarlo al arrelgo
-      if(isset($request->domCheck)){
+      if (isset($request->domCheck)) {
         $horarios[0] = $request->domOpen."-".$request->domClose;
-      }else{
+      } else {
         $horarios[0] = "Cerrado";
       }
-      if(isset($request->lunCheck)){
+      if (isset($request->lunCheck)) {
         $horarios[1] = $request->lunOpen."-".$request->lunClose;
-      }else{
+      } else {
         $horarios[1] = "Cerrado";
       }
-      if(isset($request->marCheck)){
+      if (isset($request->marCheck)) {
         $horarios[2] = $request->marOpen."-".$request->marClose;
-      }else{
+      } else {
         $horarios[2] = "Cerrado";
       }
-      if(isset($request->mieCheck)){
+      if (isset($request->mieCheck)) {
         $horarios[3] = $request->mieOpen."-".$request->mieClose;
-      }else{
+      } else {
         $horarios[3] = "Cerrado";
       }
-      if(isset($request->jueCheck)){
+      if (isset($request->jueCheck)) {
         $horarios[4] = $request->jueOpen."-".$request->jueClose;
-      }else{
+      } else {
         $horarios[4] = "Cerrado";
       }
-      if(isset($request->vieCheck)){
+      if (isset($request->vieCheck)) {
         $horarios[5] = $request->vieOpen."-".$request->vieClose;
-      }else{
+      } else {
         $horarios[5] = "Cerrado";
       }
-      if(isset($request->sabCheck)){
+      if (isset($request->sabCheck)) {
         $horarios[6] = $request->sabOpen."-".$request->sabClose;
-      }else{
+      } else {
         $horarios[6] = "Cerrado";
       }
       $cerveceria->horario=$horarios;
 
       // Get lat and long by address
-      $address = $request->direccion.", Bahía Blanca"; // Google HQ
-      $prepAddr = str_replace(' ','+',$address);
-      $geocode=file_get_contents('https://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&sensor=false');
-      $output= json_decode($geocode);
-      if(isset($output->status) && ($output->status == 'OK')){
+      $address = $request->direccion.", B8000 Bahía Blanca, Buenos Aires"; // Google HQ
+      $prepAddr = preg_replace("/\s+/", "+", $address);
+      $url = "https://maps.google.com/maps/api/geocode/json?address=".$prepAddr;
+      $geocode = file_get_contents($url);
+      $output = json_decode($geocode);
+      if (isset($output->status) && ($output->status == 'OK')) {
         $latitude = $output->results[0]->geometry->location->lat;
         $longitude = $output->results[0]->geometry->location->lng;
         $cerveceria->latLong = array($latitude,$longitude);
-     }else{
+      } else {
        //deberia tirar un error por dirección invalida?
-     }
+      }
       $cerveceria->save();
-      return 'Saved';
+      return redirect('admin');
     }
 
     /**
@@ -93,9 +95,7 @@ class AddController extends Controller
     *
     * @return void
     */
-   public function __construct()
-   {
+   public function __construct() {
        $this->middleware('auth');
-
    }
 }
