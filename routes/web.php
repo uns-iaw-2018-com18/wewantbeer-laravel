f117253a6edb3b220ec4e66238b8da8407668b84
DamianNud <DamianNud@users.noreply.github.com>
2018-06-10 19:21:28 -0300

    Algunas correcciones

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,3 @@ Route::post('/checkid', ['uses' => 'AddController@checkId']);
 Route::get('login', 'Auth\LoginController@showLoginForm') -> name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout') -> name('logout');
-
-// Registration Routes...
-Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Auth\RegisterController@register');
